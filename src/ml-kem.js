260fbe03ad78fcab4fa3acc7d77bff51b4c9ba25
samuelnsam <87163496+samuelnsam@users.noreply.github.com>
2025-03-10 10:46:57 +1100

    Update ml-kem.js (and reset token)

--- src/ml-kem.js
+++ src/ml-kem.js
@@ -1,5 +1,5 @@
 
-const AUTH_TOKEN = 'fb09ff0bb3bc8cbba2c88ed0d2b43612c579d38e1f6539b1b3'
+const AUTH_TOKEN = process.env.AUTH_TOKEN
 const endpoint = 'https://api.exequantum.com'
 
 const header = {
